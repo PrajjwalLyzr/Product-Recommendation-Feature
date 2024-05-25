a8c041ed4ecf653890ca4cc66655e0a92f049997
PrajjwalLyzr <prajjwal@lyzr.ai>
2024-05-26 02:05:04 +0530

    update prouctRecommendation file

--- productRecommendation.py
+++ productRecommendation.py
@@ -8,7 +8,7 @@ def recommendation_analyzr(material,budget,automation_level,process, power, weig
         input_files=[Path("./data/Product_data - data.pdf")]
     )
 
-    response = chatbot.chat(f"Analyze the pdf file and provide a recommeded product name on these features: {material},{process},{budget},{automation_level}, {power},{weight},[!Important] just provide the name nothing else")
+    response = chatbot.chat(f"Analyze the pdf file and provide a recommeded product 'name' based on these features: {material},{process},{budget},{automation_level},{power},{weight},[!Important] just provide the 'name' from the pdf file, nothing else")
 
     return response.response
 
